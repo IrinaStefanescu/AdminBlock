eafec602f8e1da0b4659464ff9b3246ced2354ed
IrinaStefanescu <49494590+IrinaStefanescu@users.noreply.github.com>
2022-04-29 14:01:18 +0300

    refactored profile UI

--- lib/user/profile.dart
+++ lib/user/profile.dart
@@ -41,142 +41,246 @@ class _ProfileState extends State<Profile> {
       ),
       body: Stack(
         children: [
-          Center(
-            child: ClipPath(
-              clipper: MyClipper(),
-              child: Container(
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Image.asset('lib/assets/images/verify_email.png'),
-                    Text(
-                      'Email: $email',
-                      style: GoogleFonts.inter(
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Image.asset('lib/assets/images/verify_email.png'),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 50.0),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.email_rounded,
+                              color: Colors.brown,
+                            ),
+                            Text(
+                              'Email',
+                              style: GoogleFonts.inter(
+                                color: Colors.grey[800],
+                                fontWeight: FontWeight.w400,
+                                fontSize: 17,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ],
+                        ),
                       ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          '$email',
+                          style: GoogleFonts.inter(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.w600,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Creation time: ${creationTime.toString().substring(0, 10)}',
-                      style: GoogleFonts.inter(
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.remove_red_eye,
+                          color: Colors.brown,
+                        ),
+                        Text(
+                          'Creation time:',
+                          style: GoogleFonts.inter(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.w400,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Last seen: ${DateTime.now().toString().substring(0, 10)}',
-                      style: GoogleFonts.inter(
-                        color: Colors.grey[800],
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17,
-                      ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 50.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          '${creationTime.toString().substring(0, 10)}',
+                          style: GoogleFonts.inter(
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.w700,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
                     ),
-                    StreamBuilder(
-                      stream: FirebaseAuth.instance.authStateChanges(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState !=
-                            ConnectionState.active) {
-                          return Center(child: CircularProgressIndicator());
-                        }
-                        final user = FirebaseAuth.instance.currentUser;
-                        final uid = user!.uid;
-                        if (user != null) {
-                          print(user);
+                  ),
+                  StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState != ConnectionState.active) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      final user = FirebaseAuth.instance.currentUser;
+                      final uid = user!.uid;
+                      if (user != null) {
+                        print(user);
 
-                          CollectionReference users = FirebaseFirestore.instance
-                              .collection('users_data');
+                        CollectionReference users =
+                            FirebaseFirestore.instance.collection('users_data');
 
-                          return FutureBuilder<DocumentSnapshot>(
-                            future: users.doc(uid).get(),
-                            builder: (BuildContext context,
-                                AsyncSnapshot<DocumentSnapshot> snapshot) {
-                              if (snapshot.hasError) {
-                                return Text("Something went wrong");
-                              }
+                        return FutureBuilder<DocumentSnapshot>(
+                          future: users.doc(uid).get(),
+                          builder: (BuildContext context,
+                              AsyncSnapshot<DocumentSnapshot> snapshot) {
+                            if (snapshot.hasError) {
+                              return Text("Something went wrong");
+                            }
 
-                              if (snapshot.hasData && !snapshot.data!.exists) {
-                                return Text("Document does not exist");
-                              }
+                            if (snapshot.hasData && !snapshot.data!.exists) {
+                              return Text("Document does not exist");
+                            }
 
-                              if (snapshot.connectionState ==
-                                  ConnectionState.done) {
-                                Map<String, dynamic> data = snapshot.data!
-                                    .data() as Map<String, dynamic>;
-                                return Padding(
-                                  padding:
-                                      const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                                  child: Center(
-                                    child: Column(
-                                      children: [
-                                        Text(
-                                          "${data['name']}",
-                                          style: GoogleFonts.inter(
-                                            color: Colors.grey[800],
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 17,
+                            if (snapshot.connectionState ==
+                                ConnectionState.done) {
+                              Map<String, dynamic> data =
+                                  snapshot.data!.data() as Map<String, dynamic>;
+                              return Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: Center(
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 50.0),
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              Icons.pin_drop,
+                                              color: Colors.brown,
+                                            ),
+                                            Text(
+                                              "Address",
+                                              style: GoogleFonts.inter(
+                                                color: Colors.grey[800],
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 17,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 50.0),
+                                            child: Text(
+                                              "${data['street']} street "
+                                              "${data['streetNumber']}, "
+                                              "building ${data['building']}, "
+                                              "\napartment ${data['apartment']}",
+                                              style: GoogleFonts.inter(
+                                                color: Colors.grey[800],
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 17,
+                                              ),
+                                            ),
                                           ),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 50.0),
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              Icons.contacts,
+                                              color: Colors.brown,
+                                            ),
+                                            Text(
+                                              "Name:",
+                                              style: GoogleFonts.inter(
+                                                color: Colors.grey[800],
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 17,
+                                              ),
+                                            ),
+                                          ],
                                         ),
-                                        SizedBox(
-                                          height: 2,
-                                        ),
-                                        Text(
-                                          "${data['street']} street, no ${data['streetNumber']}",
-                                          style: GoogleFonts.inter(
-                                            color: Colors.grey[800],
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 17,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 50.0),
+                                            child: Text(
+                                              "${data['name']}",
+                                              style: GoogleFonts.inter(
+                                                color: Colors.grey[800],
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 17,
+                                              ),
+                                            ),
                                           ),
+                                        ],
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 50.0),
+                                        child: Row(
+                                          children: [
+                                            Icon(
+                                              Icons.person_sharp,
+                                              color: Colors.brown,
+                                            ),
+                                            Text(
+                                              "Username:",
+                                              style: GoogleFonts.inter(
+                                                color: Colors.grey[800],
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 17,
+                                              ),
+                                            ),
+                                          ],
                                         ),
-                                        SizedBox(
-                                          height: 2,
-                                        ),
-                                        Text(
-                                          "Building ${data['building']}",
-                                          style: GoogleFonts.inter(
-                                            color: Colors.grey[800],
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 17,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 50.0),
+                                            child: Text(
+                                              "${data['username']}",
+                                              style: GoogleFonts.inter(
+                                                color: Colors.grey[800],
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 17,
+                                              ),
+                                            ),
                                           ),
-                                        ),
-                                        SizedBox(
-                                          height: 2,
-                                        ),
-                                        Text(
-                                          "Apartment ${data['apartment']}",
-                                          style: GoogleFonts.inter(
-                                            color: Colors.grey[800],
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 17,
-                                          ),
-                                        ),
-                                        Text(
-                                          "Username: ${data['username']}",
-                                          style: GoogleFonts.inter(
-                                            color: Colors.grey[800],
-                                            fontWeight: FontWeight.w500,
-                                            fontSize: 17,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+                                        ],
+                                      ),
+                                    ],
                                   ),
-                                );
-                              }
-                              return Container();
-                            },
-                          );
-                        } else {
-                          return Text("user is not logged in");
-                        }
-                      },
-                    ),
-                  ],
-                ),
+                                ),
+                              );
+                            }
+                            return Container();
+                          },
+                        );
+                      } else {
+                        return Text("user is not logged in");
+                      }
+                    },
+                  ),
+                ],
               ),
             ),
           ),
