3822209aa139931c7be0030f7012df2ab4c147a5
IrinaStefanescu <49494590+IrinaStefanescu@users.noreply.github.com>
2022-05-02 12:26:16 +0300

    finished complaints screen with calendar functionality

--- lib/user/complaints.dart
+++ lib/user/complaints.dart
@@ -1,4 +1,6 @@
 import 'package:admin_block/pages/user_main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
@@ -11,7 +13,22 @@ class Complaints extends StatefulWidget {
 }
 
 class _ComplaintsState extends State<Complaints> {
+  var name = "";
+  var issue = "";
   String _selectedDate = 'Tap to select date';
+
+  final _compliantsFormKey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final issueController = TextEditingController();
+
+  final email = FirebaseAuth.instance.currentUser!.email;
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    issueController.dispose();
+    super.dispose();
+  }
 
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? d = await showDatePicker(
@@ -40,6 +57,10 @@ class _ComplaintsState extends State<Complaints> {
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference users_compliants =
+        FirebaseFirestore.instance.collection('users_compliants');
+    final user = FirebaseAuth.instance.currentUser;
+
     return Scaffold(
       appBar: AppBar(
         shape: RoundedRectangleBorder(
@@ -70,7 +91,8 @@ class _ComplaintsState extends State<Complaints> {
         shadowColor: Colors.orange,
         automaticallyImplyLeading: false,
       ),
-      body: SingleChildScrollView(
+      body: Form(
+        key: _compliantsFormKey,
         child: Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
@@ -78,7 +100,7 @@ class _ComplaintsState extends State<Complaints> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               SizedBox(
-                height: 30,
+                height: 50,
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 1.2,
@@ -93,18 +115,140 @@ class _ComplaintsState extends State<Complaints> {
                 ),
               ),
               SizedBox(
-                height: 40,
+                height: 50,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 35.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Enter your name',
+                      style: GoogleFonts.inter(
+                        color: Colors.grey[500],
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5,
               ),
               Container(
                 width: MediaQuery.of(context).size.width / 1.2,
-                child: Text(
-                  'Open the calendar and choose the date \nthe incident happened.',
-                  style: GoogleFonts.inter(
-                    color: Colors.grey.shade900,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 17,
+                child: TextFormField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    fillColor: Color(0x70E0E0E0),
+                    filled: true,
+                    focusColor: Colors.orange,
+                    hoverColor: Colors.orange,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      ),
+                    ),
+                    errorStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 15.0,
+                    ),
                   ),
+                  controller: nameController,
+                  onChanged: (value) {
+                    name = value;
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please provide your name';
+                    }
+                    return null;
+                  },
                 ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 35.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'Enter your issue',
+                      style: GoogleFonts.inter(
+                        color: Colors.grey[500],
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                child: TextFormField(
+                  maxLines: 8,
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    fillColor: Color(0x70E0E0E0),
+                    filled: true,
+                    focusColor: Colors.orange,
+                    hoverColor: Colors.orange,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                      borderSide: BorderSide(
+                        color: Colors.orange,
+                        width: 2,
+                      ),
+                    ),
+                    errorStyle: TextStyle(
+                      color: Colors.black26,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                  controller: issueController,
+                  onChanged: (value) {
+                    issue = value;
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please provide your name';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Open the calendar and choose the date \nthe incident happened',
+                style: GoogleFonts.inter(
+                  color: Colors.grey.shade900,
+                  fontWeight: FontWeight.normal,
+                  fontSize: 17,
+                ),
+                textAlign: TextAlign.center,
               ),
               SizedBox(
                 height: 10,
@@ -144,6 +288,61 @@ class _ComplaintsState extends State<Complaints> {
                       },
                     ),
                   ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2.2,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.grey[700],
+                  ),
+                  child: TextButton(
+                    child: Text(
+                      'Submit',
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 19,
+                      ),
+                    ),
+                    onPressed: () async {
+                      if (_compliantsFormKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(
+                              'Sending data...',
+                              style: GoogleFonts.inter(
+                                color: Colors.white,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                        );
+                        await users_compliants
+                            .doc(user!.uid)
+                            .set({
+                              'name': name,
+                              'email': email,
+                              'issue': issue,
+                              'compliant_date': _selectedDate,
+                            })
+                            .then((value) => print("User's data added"))
+                            .catchError(
+                                (error) => print('Failed to add user: $error'));
+                        print("Name: $name");
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Complaints()));
+                      }
+                    },
+                  ),
                 ),
               ),
             ],
